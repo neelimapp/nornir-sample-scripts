e477200475a4415387eba2b9c1ed86b86c5d2f64
Neelima Parakala <neelimaparakala23@gmail.com>
2021-03-30 14:33:00 -0700

    Execute multiple napalm tasks

---
+++ sample3/nornir_main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+  
+from nornir import InitNornir
+from nornir_utils.plugins.functions import print_result
+from nornir_napalm.plugins.tasks import napalm_get, napalm_cli, napalm_configure
+from nornir.core.task import Task
+
+nr = InitNornir(
+    config_file="config.yaml", dry_run=True
+)
+
+def multiple_tasks(task: Task):
+    task.run(
+        task=napalm_cli, commands=["show interfaces brief"]
+    )
+
+    task.run(
+        task=napalm_configure, dry_run=False, configuration="interface loopback 1000"
+    )
+
+    task.run(
+        task=napalm_get, getters=["interfaces"]
+    )
+
+results = nr.run(
+    task=multiple_tasks
+)
+
+print_result(results)
